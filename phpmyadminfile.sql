a101aec036f8ae7712edbc1feddac2430ddb8753
Pragathi S Rao <115483497+pragathi916@users.noreply.github.com>
2024-04-26 00:09:40 +1000

    Create phpmyadminfile.sql

---
+++ phpmyadminfile.sql
@@ -0,0 +1,38 @@
+create database organ;
+
+CREATE TABLE registration (
+    username VARCHAR(20) PRIMARY KEY,
+    password varchar(20),
+    email varchar(50),
+usertype varchar(20),
+);
+CREATE TABLE login_details (
+    username VARCHAR(20) PRIMARY KEY,
+    password varchar(20),
+    login_time datetime,
+usertype varchar(20)
+    FOREIGN KEY (username) REFERENCES registration(username) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE patients (
+    username VARCHAR(20) PRIMARY KEY,
+    place	varchar(100),
+	blood_group	varchar(10),
+	contact	varchar(20)	,
+	organ	varchar(10),
+	age	int(3),
+	usertype	varchar(20)	,
+    FOREIGN KEY (username) REFERENCES registration(username) ON DELETE CASCADE ON UPDATE CASCADE
+
+);
+
+CREATE TABLE donor (
+    username VARCHAR(20) PRIMARY KEY,
+    place	varchar(100),
+	blood_group	varchar(10),
+	contact	varchar(20)	,
+	organ	varchar(10),
+	age	int(3),
+	usertype	varchar(20)	,
+    FOREIGN KEY (username) REFERENCES registration(username) ON DELETE CASCADE ON UPDATE CASCADE
+);
